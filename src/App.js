3035c6a3814ba9a7655b646cbeb45bb9c0f4dbaf
wajih <bouali.wajih@gmail.com>
2021-03-10 11:59:57 +0100

    first react

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import './App.css';
+import {Button} from 'react-bootstrap'
+import {Form , Nav , NavDropdown , FormControl , Navbar} from 'react-bootstrap';
+function App() {
+  return (
+    <div className="App">
+      <Navbar bg="light" expand="lg" >
+  <Navbar.Brand href="#home">Chat</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#link">Profil</Nav.Link>
+      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+  
+
+    <Form inline className="formss">
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-success">Search</Button>
+    </Form>
+  </Navbar.Collapse>
+</Navbar>
+<Form>
+  <Form.Group controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Connect
+  </Button>
+</Form> 
+<img src="https://i.pinimg.com/originals/94/dc/0a/94dc0acf3fac499356088f3d79eb1aaa.png"alt="pic" />
+   </div>
+  );
+}
+
+export default App;
